50673b18c015ebbd60e4b156ad6501f97823f6bd
Brett Bender <brett@brettb.xyz>
2020-01-26 18:22:45 -0600

    Version bump

--- Shared/consts.go
+++ Shared/consts.go
@@ -22,7 +22,7 @@ type Permission int
 
 const (
 	COLOR   int    = 0x532c60
-	VERSION string = "v0.10.0"
+	VERSION string = "v0.11.0"
 	BOTTAG  string = "<:bot:671061944358142007>"
 
 	PermissionAdministrator       Permission = 8
