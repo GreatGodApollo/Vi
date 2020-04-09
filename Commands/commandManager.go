d34e2ba74d807041f3f345471b844f1c014ab77e
Brett Bender <brett@brettb.xyz>
2020-04-09 11:07:48 -0500

    fix: Crash on no arguments

--- Commands/commandManager.go
+++ Commands/commandManager.go
@@ -174,7 +174,7 @@ func (cmdm *CommandManager) CommandHandler(s *discordgo.Session, m *discordgo.Me
 		if command.ProcessArgs != nil {
 			args = command.ProcessArgs(cmd[1:])
 		} else {
-			args = cmd[1]
+			args = cmd[1:]
 		}
 
 		ctx := CommandContext{
--- Shared/consts.go
+++ Shared/consts.go
@@ -22,7 +22,7 @@ type Permission int
 
 const (
 	COLOR   int    = 0x532c60
-	VERSION string = "v0.21.0"
+	VERSION string = "v0.22.0"
 
 	REQUIREDPERMISSIONS Permission = PermissionMessagesManage | PermissionMessagesEmbedLinks |
 		PermissionMessagesSend | PermissionMessagesReadHistory | PermissionViewChannels |
